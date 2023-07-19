c77b1e4d421fdc0f84dee29185b99c771c865604
vianat <vianatdev@gmail.com>
2023-07-19 13:22:49 -0700

    long press

--- src/test/java/org/example/Base.java
+++ src/test/java/org/example/Base.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import io.appium.java_client.android.AndroidDriver;
@@ -41,12 +45,17 @@ public class Base {
 
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("MyEmu");
-        options.setApp(projectDir + "//src//test//java//resources//calk.apk");
+        options.setApp(projectDir + "//src//test//java//resources//ApiDemos-debug.apk");
 
 //      driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), options);
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
     }
 
+    public void longPressOnElement(WebElement el){
+        ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",
+                ImmutableMap.of("elementId", ((RemoteWebElement)el).getId(),
+                        "duration", 2000));
+    }
 
     @AfterClass
     public void tearDown(){
---
+++ src/test/java/org/example/LongPress.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+
+public class LongPress extends Base{
+    @Test
+    public void longPress() throws MalformedURLException, InterruptedException {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
+        driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+
+        WebElement el = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ExpandableListView/android.widget.TextView[1]"));
+
+        longPressOnElement(el);
+
+        Thread.sleep(2000);
+
+        String menuText = driver.findElement(By.id("android:id/title")).getText();
+        Assert.assertEquals(menuText, "Sample menu");
+        Assert.assertTrue(driver.findElement(By.id("android:id/title")).isDisplayed());
+    }
+
+}
