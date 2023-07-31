c13094c3decec42e1c0e8ef0a84b4be17a7485ca
vianat <vianatdev@gmail.com>
2023-07-31 14:55:32 -0700

    add BT test

--- src/test/java/steps/GoogleTest.java
+++ src/test/java/steps/GoogleTest.java
@@ -2,16 +2,15 @@ package steps;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.Activity;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 public class GoogleTest extends Base {
-
-    // You can open any page off app by using activity and avoid many steps of navigation
-    // https://support.testsigma.com/support/solutions/articles/32000019977-how-to-find-app-package-and-app-activity-of-your-android-app
-    // adb shell dumpsys window | grep -E 'mCurrentFocus'
 
     @Given("open settings")
     public void open_settings() {
@@ -24,19 +23,24 @@ public class GoogleTest extends Base {
         Assert.assertEquals(actual, "Settings");
     }
 
-    @Given("open wifi settings")
-    public void open_wifi_settings() throws InterruptedException {
+    @And("click on {string}")
+    public void clickOn(String locator) throws InterruptedException {
+        driver.findElement(AppiumBy.xpath("//*[@text='"+ locator +"']")).click();
+        Thread.sleep(50);
+    }
 
-        driver.findElement(AppiumBy.xpath("//*[@text='Network & internet']")).click();
-        Thread.sleep(100);
-        driver.findElement(AppiumBy.xpath("//*[@text='Internet']")).click();
-        Thread.sleep(100);
-
+    @When("Turn on switch")
+    public void turnOnSwitch() {
         WebElement el = driver.findElement(AppiumBy.xpath("//*[@class='android.widget.Switch']"));
 
         if (el.getAttribute("checked").equals("true")) {
         } else el.click();
+    }
 
+    @Then("Check switch is on")
+    public void checkSwitchIsOn() {
+        WebElement el = driver.findElement(AppiumBy.xpath("//*[@class='android.widget.Switch']"));
         Assert.assertEquals(el.getAttribute("checked"), "true");
     }
+
 }
