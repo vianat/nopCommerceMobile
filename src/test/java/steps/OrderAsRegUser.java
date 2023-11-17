d83f26f325e9d4288d0d78e78d5981825dbe08ed
Azik1994 <utr1994@mail.ru>
2023-11-16 23:37:02 -0800

    230

---
+++ src/test/java/steps/OrderAsRegUser.java
@@ -0,0 +1,87 @@
+package steps;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebElement;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class OrderAsRegUser extends Base {
+
+    WebElement electronics;
+    WebElement AddToCart;
+    WebElement CART;
+    WebElement cameraAndPhoto;
+    WebElement product;
+    WebElement deleteProduct;
+    WebElement HOME;
+    WebElement MENU;
+    WebElement USER;
+
+
+    @Given("Open NopSuite mobile application")
+    public void openNopSuiteMobileApplication() {
+        Activity activity = new Activity("com.nopadvance.team", "com.nopadvance.team.MainActivity");
+        driver.startActivity(activity);
+    }
+
+    @Given("MAIN buttons initializing")
+    public void mainButtonsInitializing() throws InterruptedException {
+        Thread.sleep(300);
+        HOME = driver.findElement(AppiumBy.xpath("//android.widget.ImageView[1]"));
+        Thread.sleep(300);
+        MENU = driver.findElement(AppiumBy.xpath("//android.widget.ImageView[2]"));
+        Thread.sleep(300);
+        CART = driver.findElement(AppiumBy.xpath("//android.widget.ImageView[3]"));
+        Thread.sleep(300);
+        USER = driver.findElement(AppiumBy.xpath("//android.widget.ImageView[4]"));
+    }
+
+    @And("Tap Add To Cart button")
+    public void tapAddToCartButton() throws InterruptedException {
+        Thread.sleep(1000);
+        AddToCart = driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc='ADD TO CART']"));
+        AddToCart.click();
+    }
+
+    @And("Tap Shopping Cart button")
+    public void tapShoppingCartButton() throws InterruptedException {
+        Thread.sleep(1000);
+        CART = driver.findElement(AppiumBy.xpath("//android.widget.ImageView[3]"));
+        CART.click();
+    }
+
+    @And("Tap on Electronics")
+    public void tapOnElectronics() {
+        electronics = driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc='ELECTRONICS']"));
+        electronics.click();
+    }
+
+    @And("Tap on Camera&Photo")
+    public void tapOnCameraPhoto() {
+        cameraAndPhoto = driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc='CAMERA & PHOTO']"));
+        cameraAndPhoto.click();
+    }
+
+    @And("Tap on any product")
+    public void tapOnAnyProduct() {
+        product = driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc='Leica T Mirrorless Digital Camera']"));
+        product.click();
+    }
+
+    @And("Tap delete button")
+    public void tapDeleteButton() throws InterruptedException {
+        Thread.sleep(1000);
+        deleteProduct = driver.findElement(AppiumBy.xpath("(//android.widget.ScrollView//android.view.View)[8]"));
+        deleteProduct.click();
+    }
+
+    @And("Tap USER button")
+    public void tapUSERButton() throws InterruptedException {
+        Thread.sleep(1000);
+        USER= driver.findElement(AppiumBy.xpath("//android.widget.ImageView[4]"));
+        USER.click();
+    }
+}
